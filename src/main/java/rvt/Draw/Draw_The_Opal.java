f49c6c4f4621e91003141f2677fa52fd7133fccd
Eric Vansovich <A240150EV@rvt.lv>
2026-03-24 12:48:20 +0200

    Add Draw_The_Opal class to create a simple JFrame window

---
+++ src/main/java/rvt/Draw/Draw_The_Opal.java
@@ -0,0 +1,13 @@
+package rvt.Draw;
+
+import javax.swing.JFrame;
+
+public class Draw_The_Opal {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Java grafika");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(200, 200);
+        frame.setVisible(true);
+    }
+}
